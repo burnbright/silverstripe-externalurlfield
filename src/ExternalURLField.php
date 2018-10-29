f4c415105aad603e5bb6810042ed809f46c709fe
Luke Fromhold <luke@fromhold.io>
2018-10-29 15:38:08 +1100

    Update field setValue to resolve E_STRICT notice 'declaration of methods should be compatible with parent methods in php'

--- src/ExternalURLField.php
+++ src/ExternalURLField.php
@@ -116,13 +116,15 @@ class ExternalURLField extends TextField
     /**
      * Rebuild url on save
      * @param string $url
+     * @param array|DataObject $data {@see Form::loadDataFrom}
+     * @return $this
      */
-    public function setValue($url)
+    public function setValue($url, $data = null)
     {
         if ($url) {
             $url = $this->rebuildURL($url);
         }
-        parent::setValue($url);
+        parent::setValue($url, $data);
     }
 
     /**
